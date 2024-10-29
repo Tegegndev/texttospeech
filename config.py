5e901aaf35217edf9047d323205519c4090da028
Tegegn Wukianos <146325314+Tegegnwukianos@users.noreply.github.com>
2024-10-29 04:32:15 -0700

    Initial commit with cleaned history

---
+++ config.py
@@ -0,0 +1,3 @@
+DETECT_LANG_API_KEY = 'c1a297afcff49036cae7893298de3a0f'
+BOT_TOKEN = BOT_TOKEN = '7676184950:AAHhJcNASGzkNzuGpAjnmoK-YZYIpQOI6jg'
+TEXT_TO_SPEECH_API_URL = "https://text-to-speech.manzoor76b.workers.dev/"
